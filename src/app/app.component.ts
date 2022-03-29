427f9169ca2081010639e46c1269965814f78e50
JimmyGla <balbo_angus_young@live.it>
2022-03-29 21:45:58 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JokesService } from './jokes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'chuck-api';
+export class AppComponent implements OnInit {
+  jokes: any[] = [];
+  categories: any[] = [];
+  title: any;
+
+  constructor(private jokeService: JokesService) {}
+
+  ngOnInit() {
+    this.jokeService.getCategories().subscribe((categories: any) => {
+      this.categories = categories;
+
+      this.jokeService.getRandomJoke().subscribe((joke) => {
+        this.jokes.push(joke);
+      });
+    });
+  }
+
+  // Search By Category
+  searchByCategory(category: string) {
+    this.jokes = [];
+    for (let i = 0; i < 5; i++) {
+      this.jokeService.getCategoryJoke(category).subscribe((joke) => {
+        this.jokes.push(joke);
+      });
+    }
+  }
 }
---
+++ src/app/jokes.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JokesService {
+  private apiUrl = 'https://api.chucknorris.io/jokes/';
+
+  constructor(private http: HttpClient) {}
+
+  // Get Random Joke
+  getRandomJoke() {
+    return this.http.get(this.apiUrl + 'random');
+  }
+
+  // Get Categories
+  getCategories() {
+    return this.http.get(this.apiUrl + 'categories');
+  }
+
+  // Get Joke By Category
+  getCategoryJoke(category: string) {
+    return this.http.get(this.apiUrl + `random?category=${category}`);
+  }
+}
